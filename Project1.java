78dab18279943bb85dabe639bcaede110e15c8d6
Pavan <pkg190000@utdallas.edu>
2021-03-09 17:42:24 -0600

    changed name of main cpu file

--- Project1.java
+++ Project1.java
@@ -1,31 +1,13 @@
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
-
 public class Project1
 {
-   public static void main(String args[])throws Exception
-   {
-      String inputFileName;//holds input sample.txt file
-      int timerInterruptValue;//value for interrupt timer
-   
-      //filename and timer interrupt value arguments required
-      if(args.length <2)
-      {
-         System.out.println("Insufficient Arguments! Please include filename and timer-interrupt value.");
-         System.exit(0);//kill the program
-      }
-      
-      //parse command line arguments
-      inputFileName=args[0];
-      timerInterruptValue=Integer.parseInt(args[1]);//String to int conversion
-      
-      //open input file
-      File file = new File(inputFileName);
-      Scanner input = new Scanner(file);
-      
-      System.out.println(input.nextLine());
-      
-      //Runtime class allows application to interface with environment
-      Runtime currentRunTime = Runtime.getRuntime();
-   }
+public static void main(String[] args)
+{
+if(args.length < 2)
+{
+System.err.println("Not enough arguments (requires input program and timer length)");
+System.exit(0);
 }
