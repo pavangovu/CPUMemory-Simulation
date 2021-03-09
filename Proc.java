4f975500b412990d9c6bac9035cca30ba21e70b7
Pavan <pkg190000@utdallas.edu>
2021-03-09 12:14:22 -0600

    beginning

--- Proc.java
+++ Proc.java
@@ -8,28 +8,28 @@ public class Proc
    {
       try
       {            
-	 int x;
-	 Runtime rt = Runtime.getRuntime();
-
-	 Process proc = rt.exec("java Hello");
-	 //Process proc = rt.exec("cat hello.java");
-
-	 InputStream is = proc.getInputStream();
-	 OutputStream os = proc.getOutputStream();
-
-	 while ((x=is.read()) != -1)
-	    System.out.println((char)x); 
-	      
-	 proc.waitFor();
-
+         int x;
+         Runtime rt = Runtime.getRuntime();
+      
+         Process proc = rt.exec("java Hello");
+      //Process proc = rt.exec("cat hello.java");
+      
+         InputStream is = proc.getInputStream();
+         OutputStream os = proc.getOutputStream();
+      
+         while ((x=is.read()) != -1)
+            System.out.println((char)x); 
+         
+         proc.waitFor();
+      
          int exitVal = proc.exitValue();
-
+      
          System.out.println("Process exited: " + exitVal);
-
+      
       } 
       catch (Throwable t)
       {
-	 t.printStackTrace();
+         t.printStackTrace();
       }
    }
 }
--- Proc2.java
+++ Proc2.java
@@ -9,34 +9,34 @@ public class Proc2
    {
       try
       {            
-	 int x;
-	 Runtime rt = Runtime.getRuntime();
-
-	 Process proc = rt.exec("java HelloYou");
-
-	 InputStream is = proc.getInputStream();
-	 OutputStream os = proc.getOutputStream();
-
+         int x;
+         Runtime rt = Runtime.getRuntime();
+      
+         Process proc = rt.exec("java HelloYou");
+      
+         InputStream is = proc.getInputStream();
+         OutputStream os = proc.getOutputStream();
+      
          PrintWriter pw = new PrintWriter(os);
-	 pw.printf("Greg\n");
+         pw.printf("Greg\n");
          pw.flush();
-
-	 Scanner sc = new Scanner(is);
-	 String line = sc.nextLine();
-
+      
+         Scanner sc = new Scanner(is);
+         String line = sc.nextLine();
+      
          for (int i=0; i<line.length(); i++)
-	    System.out.println(line.charAt(i)); 
-	      
-	 proc.waitFor();
-
+            System.out.println(line.charAt(i)); 
+         
+         proc.waitFor();
+      
          int exitVal = proc.exitValue();
-
+      
          System.out.println("Process exited: " + exitVal);
-
+      
       } 
       catch (Throwable t)
       {
-	 t.printStackTrace();
+         t.printStackTrace();
       }
    }
 }
